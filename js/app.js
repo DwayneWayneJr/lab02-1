34c89e9cc1734de81a8210aafb1640e784f052f5
littl3birdy <ashlynmmorin@gmail.com>
2019-09-10 17:15:20 -0700

    Added JS and CSS

--- js/app.js
+++ js/app.js
@@ -1,21 +1,55 @@
 'use strict'
 
-var answer = confirm('Are you ready?')
-if(answer === true){
-    alert('Yay!');
-} else {
-    alert('Hurry!');
+var name = prompt('What is your name?');
+    alert('Hello '+ name + '!  Welcome to my bio! How about a quiz?');
+    console.log('Your name is ' + name + '.');
+
+var kids = prompt('Yes or no: Do I have 5 kids?');
+if(kids.toLowerCase()=== 'yes'|| kids.toLowerCase()==='y'){
+    alert('Yep! Crazy, right?');
+}else if(kids.toLowerCase()=== 'no' || kids.toLowerCase()=== 'n'){
+    alert('Wrong! I do indeed have 5 kids.');
+}else {
+    alert('Invalid Answer.');
 }
+    console.log('I have 5 kids.');
 
-var lunch = confirm('Did you eat lunch?');
-var hungry = confirm('Are you hungry?');
-
-if (lunch && hungry){
-    alert('Eat some more!');
-} else {
-    alert ('Good!');
+var coffee = prompt('Yes or no: Do I like coffee?');
+if(coffee.toLowerCase()=== 'yes' || coffee.toLowerCase()=== 'y'){
+    alert('More like "Hell to the yes!"');
+}else if (coffee.toLowerCase()=== 'no' || coffee.toLowerCase()=== 'n'){
+    alert('Are you crazy? I am probably on cup 5!');
+}else {
+    alert('Invalid Answer.');
 }
+    console.log('Coffee is life.');
 
-var iceCream = ['chocolate', 'vanilla', 'strawberry'];
-iceCream [1] = 'cherry';
-console.log(iceCream);
+var travel= prompt('Yes or no: Have I been to Spain?');
+if(travel.toLowerCase()=== 'yes' || travel.toLowerCase()=== 'y'){
+    alert('Not yet! Goals!');
+}else if (travel.toLowerCase()=== 'no' || travel.toLowerCase()=== 'n'){
+    alert('Sad, right?');
+}else {
+    alert('Invalid Answer.');
+}
+    console.log('I have yet to go to Spain. :(');
+
+var height = prompt('Yes or no: Am I afraid of heights?');
+if(height.toLowerCase()=== 'yes' || height.toLowerCase()=== 'y'){
+    alert('True, but for some reason I love scaring the shiz out of myself by jumping from high places.');
+}else if (height.toLowerCase()=== 'no' || height.toLowerCase()=== 'n'){
+    alert('Wrong answer, '+ name +'.');
+}else {
+    alert('Invalid Answer.');
+}
+    console.log('I am afraid of heights.');
+
+var drums = prompt('Yes or no: Am I a drummer?');
+if(drums.toLowerCase()=== 'yes' || drums.toLowerCase()=== 'y'){
+    alert('Yep!');
+}else if (drums.toLowerCase()=== 'no' || drums.toLowerCase()=== 'n'){
+    alert('I have been playing since I was 12!');
+}else {
+    alert('Invalid Answer.');
+}
+    console.log('I am a drummer');
