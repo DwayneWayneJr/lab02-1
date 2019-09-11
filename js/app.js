f0958ceb2207fb6d44b53769325b27b11b648a9f
littl3birdy <ashlynmmorin@gmail.com>
2019-09-11 10:02:45 -0700

    Adjusted indentation

--- js/app.js
+++ js/app.js
@@ -1,55 +1,61 @@
 'use strict'
 
 var name = prompt('What is your name?');
-    alert('Hello '+ name + '!  Welcome to my bio! How about a quiz?');
-    console.log('Your name is ' + name + '.');
+
+alert('Hello ' + name + '!  Welcome to my bio! How about a quiz?');
+console.log('Your name is ' + name + '.');
 
 var kids = prompt('Yes or no: Do I have 5 kids?');
-if(kids.toLowerCase()=== 'yes'|| kids.toLowerCase()==='y'){
+
+if (kids.toLowerCase() === 'yes' || kids.toLowerCase() === 'y') {
     alert('Yep! Crazy, right?');
-}else if(kids.toLowerCase()=== 'no' || kids.toLowerCase()=== 'n'){
+} else if (kids.toLowerCase() === 'no' || kids.toLowerCase() === 'n') {
     alert('Wrong! I do indeed have 5 kids.');
-}else {
+} else {
     alert('Invalid Answer.');
 }
-    console.log('I have 5 kids.');
+console.log('I have 5 kids.');
 
 var coffee = prompt('Yes or no: Do I like coffee?');
-if(coffee.toLowerCase()=== 'yes' || coffee.toLowerCase()=== 'y'){
-    alert('More like "Hell to the yes!"');
-}else if (coffee.toLowerCase()=== 'no' || coffee.toLowerCase()=== 'n'){
-    alert('Are you crazy? I am probably on cup 5!');
-}else {
-    alert('Invalid Answer.');
-}
-    console.log('Coffee is life.');
 
-var travel= prompt('Yes or no: Have I been to Spain?');
-if(travel.toLowerCase()=== 'yes' || travel.toLowerCase()=== 'y'){
-    alert('Not yet! Goals!');
-}else if (travel.toLowerCase()=== 'no' || travel.toLowerCase()=== 'n'){
-    alert('Sad, right?');
-}else {
+if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase() === 'y') {
+    alert('More like "Hell to the yes!"');
+} else if (coffee.toLowerCase() === 'no' || coffee.toLowerCase() === 'n') {
+    alert('Are you crazy? I am probably on cup 5!');
+} else {
     alert('Invalid Answer.');
 }
-    console.log('I have yet to go to Spain. :(');
+console.log('Coffee is life.');
+
+var travel = prompt('Yes or no: Have I been to Spain?');
+
+if (travel.toLowerCase() === 'yes' || travel.toLowerCase() === 'y') {
+    alert('Not yet! Goals!');
+} else if (travel.toLowerCase() === 'no' || travel.toLowerCase() === 'n') {
+    alert('Sad, right?');
+} else {
+    alert('Invalid Answer.');
+}
+console.log('I have yet to go to Spain. :(');
 
 var height = prompt('Yes or no: Am I afraid of heights?');
-if(height.toLowerCase()=== 'yes' || height.toLowerCase()=== 'y'){
+
+if (height.toLowerCase() === 'yes' || height.toLowerCase() === 'y') {
     alert('True, but for some reason I love scaring the shiz out of myself by jumping from high places.');
-}else if (height.toLowerCase()=== 'no' || height.toLowerCase()=== 'n'){
-    alert('Wrong answer, '+ name +'.');
-}else {
+} else if (height.toLowerCase() === 'no' || height.toLowerCase() === 'n') {
+    alert('Wrong answer, ' + name + '.');
+} else {
     alert('Invalid Answer.');
 }
-    console.log('I am afraid of heights.');
+console.log('I am afraid of heights.');
 
 var drums = prompt('Yes or no: Am I a drummer?');
-if(drums.toLowerCase()=== 'yes' || drums.toLowerCase()=== 'y'){
+
+if (drums.toLowerCase() === 'yes' || drums.toLowerCase() === 'y') {
     alert('Yep!');
-}else if (drums.toLowerCase()=== 'no' || drums.toLowerCase()=== 'n'){
+} else if (drums.toLowerCase() === 'no' || drums.toLowerCase() === 'n') {
     alert('I have been playing since I was 12!');
-}else {
+} else {
     alert('Invalid Answer.');
 }
-    console.log('I am a drummer');
+console.log('I am a drummer');
